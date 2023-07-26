d59810ebea7e1404d572fd1a80e0ea3c4d8a66f1
argenismiranda <argenismiranda@gmail.com>
2023-07-26 12:35:15 -0700

    updated

--- _atoi.c
+++ _atoi.c
@@ -1,72 +1,32 @@
 #include "shell.h"
 
 /**
- * interactive_mode - returns 1 if shell is in interactive mode, 0 otherwise
- * @info: pointer to the info_t structure
+ * _atoi - convert a string to an integer.
  *
- * Return: 1 if in interactive mode, 0 otherwise
+ * @prmString: char pointer
+ *
+ * Return: int
  */
-int interactive_mode(info_t *info)
+int _atoi(char *prmString)
 {
-	return (isatty(STDIN_FILENO) && info->readfd <= 2);
-}
+	char sign = 1, current;
+	int size = _strlen(prmString), cLoop;
+	unsigned int number = 0;
 
-/**
- * is_delimiter - checks if character is a delimiter
- * @character: character to check
- * @delim: delimiter string
- * Return: 1 if true, 0 if false
- */
-int is_delimiter(char character, char *delim)
-{
-	while (*delim)
+	for (cLoop = 0; cLoop < size; cLoop++)
 	{
-		if (*delim == character)
-			return (1);
-		delim++;
-	}
-	return (0);
-}
+		current = prmString[cLoop];
 
-/**
- * is_alphabetic - checks for alphabetic character
- * @c: The character to check
- * Return: 1 if c is alphabetic, 0 otherwise
- */
-int is_alphabetic(int c)
-{
-	return ((c >= 'b' && c <= 'x') || (c >= 'B' && c <= 'X'));
-}
-
-/**
- * _atoi - converts a string to an integer
- * @str: the string to be converted
- * Return: 0 if no numbers in string, converted number otherwise
- */
-int _atoi(char *str)
-{
-	int i, sign = 1, flag = 0, output;
-	unsigned int result = 0;
-
-	for (i = 0; str[i] != '\0' && flag != 2; i++)
-	{
-		if (str[i] == '-')
-			sign *= -1;
-
-		if (str[i] >= '0' && str[i] <= '9')
+		if (_isdigit(current))
 		{
-			flag = 1;
-			result *= 10;
-			result += (str[i] - '0');
+			number *= 10;
+			number += current - 48;
 		}
-		else if (flag == 1)
-			flag = 2;
+		else if (current == '-')
+			sign = -sign;
+		else if (number > 0)
+			break;
 	}
 
-	if (sign == -1)
-		output = -result;
-	else
-		output = result;
-
-	return (output);
+	return (sign * number);
 }
